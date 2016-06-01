4666763706150c0f56eeabb4da33e81ded431976
David Scully <davidscully129@gmail.com>
2016-06-01 09:43:27 +0100

    Initial commit

---
+++ src/test/java/com/inis/misc/TestAllureIsWorking.java
@@ -0,0 +1,13 @@
+package com.inis.misc;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class TestAllureIsWorking {
+
+	@Test
+	public void test(){
+		Assert.assertTrue(1 == 1);
+	}
+}
